ffa14674cad79c0162428dbd5820c58defe24ff5
Jaebin <wjdwoqls777@naver.com>
2023-07-28 16:11:54 +0900

    test file

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,15 +1,19 @@
-import { Button, Header } from "ui";
+import Video from "ui/Video";
+import VideoPlayer from "ui/VideoPlayer";
 
-export default function Page() {
+const Page: React.FC = () => {
   return (
     <>
-      <Header text="Web" />
-      <Header text="Web" />
-      <Header text="Web" />
-      <Header text="Hello World!" />
-      <Button />
-      <Button />
-      <Button />
+      <div>
+        {/* <VideoPlayer
+          src="https://www.youtube.com/embed/eyX5zUI3oq0"
+          width={640}
+          height={360}
+        /> */}
+        <Video src="https://www.youtube.com/embed/eyX5zUI3oq0?autoplay=0&mute=1&loop=1&playlist=Y8JFxS1HlDo&controls=0&enablejsapi=1&version=3&playerapiid=ytplayer" />
+      </div>
     </>
   );
-}
+};
+
+export default Page;
